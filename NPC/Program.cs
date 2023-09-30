ebc99183addcdc1eabb74531b8a0d0ec42c8ffe0
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2023-09-30 16:39:44 -0700

    keyboard command and basic character display
    might need to separate some functionality to smooth out main method chunkiness

--- NPC/Program.cs
+++ NPC/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Adventure
+﻿using System.Numerics;
+
+namespace Adventure
 {
     class Program
     {
@@ -9,6 +11,15 @@
             //                Objects can be identified by more than one type
             //                Ex. A Dog is also: Canine, Animal, Organism
 
+            int height = Console.WindowHeight - 1;
+            int width = Console.WindowWidth - 5;
+
+            // Available player and food strings
+            string[] states = { "('-')", "(^-^)", "(X_X)" };
+
+            // Current player string displayed in the Console
+            string player = states[0];
+
             Player hero = new("Hiro", "O", "Hero", 100, 100, 100, 5, new List<string> { "sword", "shield", "chainmail" }, new int[] {0, 0});
             Villager fisherman = new("Bob", "Smith", "fisherman", 5, 5, 5, 2, new List<string> {"fishing pole"}, new int[] { 0, 0 });
 
@@ -17,44 +28,70 @@
 
             NPC[] characters = { hero, fisherman, demon };
 
-            foreach (NPC peeps in characters)
-            {
-                peeps.DisplayStats();
-                peeps.Inventory();
-            }
+            //foreach (NPC peeps in characters)
+            //{
+            //    peeps.DisplayStats();
+            //    peeps.Inventory();
+            //}
 
             bool end = true;
-            string? command;
+
             Console.WriteLine("THE ADVENTURE BEGINS");
             do
             {
-                Console.WriteLine($"({hero.position[0]}, {hero.position[1]})");
-                command = Console.ReadLine();
-                switch(command)
+                int lastX = hero.position[0];
+                int lastY = hero.position[1];
+                switch (Console.ReadKey(true).Key)
                 {
-                    case "1": //right
-                        hero.position[0] += 1;
-                        break; 
-                    case "2": //left
-                        hero.position[0] -= 1;
-                        break;
-                    case "3": //forward
-                        hero.position[1] += 1;
-                        break;
-                    case "4": //backward
+                    case ConsoleKey.UpArrow:
                         hero.position[1] -= 1;
                         break;
-                    case "end":
+                    case ConsoleKey.DownArrow:
+                        hero.position[1] += 1;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        hero.position[0] -= 1;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        hero.position[0] += 1;
+                        break;
+                    case ConsoleKey.Escape:
                         end = false;
                         break;
+                    case ConsoleKey.P: //show current position
+                        Console.WriteLine($"({hero.position[0]}, {hero.position[1]})");
+                        break;
+                    case ConsoleKey.I:
+                        hero.Inventory();
+                        break;
+                    case ConsoleKey.S:
+                        hero.DisplayStats();
+                        break;
                     default:
-                        Console.WriteLine();
+                        Console.WriteLine("Illegal button detected!!!");
+                        end = false;
                         break;
                 }
+
+                //Clear the characters at the previous position
+                Console.SetCursorPosition(lastX, lastY);
+                for (int i = 0; i < player.Length; i++)
+                {
+                    Console.Write(" ");
+                }
+
                 if (hero.position[0] == demon.position[0] && hero.position[1] == demon.position[1])
                 {
                     Console.WriteLine($"You have encountered ");
                 }
+
+                // Keep player position within the bounds of the Terminal window
+                hero.position[0] = (hero.position[0] < 0) ? 0 : (hero.position[0] >= width ? width : hero.position[0]);
+                hero.position[1] = (hero.position[1] < 0) ? 0 : (hero.position[1] >= height ? height : hero.position[1]);
+
+                // Draw the player at the new location
+                Console.SetCursorPosition(hero.position[0], hero.position[1]);
+                Console.Write(player);
 
             } while (hero.HP > 0 && end);
 
